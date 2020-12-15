d3995860c62d004ca6556564331137fff0a1c3ec
citrusbolt <zac@citrusbolt.net>
2020-12-15 15:26:40 -0700

    Give trainer classes specific Poké Balls

--- include/battle_main.h
+++ include/battle_main.h
@@ -104,4 +104,10 @@ extern const u8 gStatusConditionString_LoveJpn[8];
 
 extern const u8 *const gStatusConditionStringsTable[7][2];
 
+struct TrainerBall
+{
+	u8 classId;
+	u8 ball;
+};
+
 #endif // GUARD_BATTLE_MAIN_H
