e91b47c267c11db96dcad0225c3afbf5e7b5dbf6
citrusbolt <zac@citrusbolt.net>
2020-12-16 14:27:00 -0700

    Remove confirmation from save menu

--- src/start_menu.c
+++ src/start_menu.c
@@ -998,7 +998,7 @@ static u8 SaveFileExistsCallback(void)
     }
     else
     {
-        ShowSaveMessage(gText_AlreadySavedFile, SaveConfirmOverwriteCallback);
+        sSaveDialogCallback = SaveSavingMessageCallback;
     }
 
     return SAVE_IN_PROGRESS;
